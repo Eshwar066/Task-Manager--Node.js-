fd15814c4ce1b332df887fe9704be37de911df57
Eshwar <eshwarsairam7@gmail.com>
2023-07-12 15:24:50 +0530

    crud1

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express =require('express');
 const app= express();
 const tasks = require('./routes/tasks')
+const mongoose=require('mongoose')
 const connectDB=require('./dp/connect')
 require('dotenv').config()
 const notFound=require('./middleware/not-found')
@@ -25,7 +26,7 @@ const port= process.env.PORT || 4000
 
 const start=async()=>{
     try{
-        await connectDB(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true, useUnifiedTopology:true})
         app.listen(port,console.log(`server is listening on port ${port}...`))
     }catch(error){
         console.log(error)
